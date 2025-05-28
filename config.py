dedee9afe2194a81af89765170c12f053ad62fe0
Jason Yap <82182700+jasonydog9@users.noreply.github.com>
2025-05-28 11:12:14 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,76 @@
+TOKEN = 'INSERT YOUR TOKEN HERE'
+
+# FFMPEG settings for audio processing
+FFMPEG_OPTIONS = {'options': '-vn'}
+
+# Discord embed colors
+COLORS = {
+    'BLUE': 0x0000FF,
+    'GREEN': 0x00FF00,
+    'RED': 0xFF0000,
+    'YELLOW': 0xFFBF00,
+    'PURPLE': 0x738ADB,
+    'ORANGE': 0x3776AB
+}
+
+# Quiz categories mapping
+CATEGORIES = {
+    "Literature": "lit",
+    "Science": "sci",
+    "History": "hist",
+    "Fine Arts": "fa",
+    "Religion": "religion",
+    "Mythology": "myth",
+    "Philosophy": "philo",
+    "Social Science": "ss",
+    "Current Events": "ce",
+    "Geography": "geo",
+    "Trash": "trash",
+    # Literature subcategories
+    "American Literature": "amlit",
+    "British Literature": "britlit",
+    "Classical Literature": "classicallit",
+    "European Literature": "eurolit",
+    "World Literature": "worldlit",
+    "Other Literature": "otherlit",
+    # History subcategories
+    "American History": "amhist",
+    "Ancient History": "ancienthist",
+    "European History": "eurohist",
+    "World History": "worldhist",
+    "Other History": "otherhist",
+    # Science subcategories
+    "Biology": "bio",
+    "Chemistry": "chem",
+    "Physics": "phys",
+    "Other Science": "osci",
+    # Fine Arts subcategories
+    "Visual Fine Arts": "vfa",
+    "Auditory Fine Arts": "afa",
+    "Other Fine Arts": "ofa",
+    # Additional categories
+    "Drama": "drama",
+    "Long Fiction": "longfiction",
+    "Poetry": "poetry",
+    "Short Fiction": "shortfiction",
+    "Misc Literature": "misclit",
+    "Math": "math",
+    "Astronomy": "astronomy",
+    "Computer Science": "cs",
+    "Earth Science": "earthscience",
+    "Engineering": "eng",
+    "Misc Science": "miscsci",
+    "Architecture": "architecture",
+    "Dance": "dance",
+    "Film": "film",
+    "Jazz": "jazz",
+    "Opera": "opera",
+    "Photography": "photography",
+    "Misc Arts": "miscarts",
+    "Anthropology": "anthropology",
+    "Economics": "economics",
+    "Linguistics": "linguistics",
+    "Psychology": "psych",
+    "Sociology": "sociology",
+    "Other Social Science": "otherss"
+}
